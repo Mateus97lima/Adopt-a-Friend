f7846d2f8258433b33f5632616b96eaa9da5bc16
Mateus lima <matheus2022y@gmail.com>
2026-03-20 16:28:33 -0300

    deixei o codigo mais limpo

--- src/utils/Pixabay.ts
+++
@@ -1,8 +0,0 @@
-// lib/pixabay.ts
-export async function fetchPixabay(query: string, perPage: number = 9) {
-  const API_KEY = process.env.API_KEY; // sua chave
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&per_page=${perPage}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.hits; // retorna array de imagens
-}
