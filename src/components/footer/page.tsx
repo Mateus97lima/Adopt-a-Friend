59f13b799c25a04bd1e25f5ced835e045969b2da
Mateus lima <matheus2022y@gmail.com>
2026-03-25 17:39:08 -0300

    ajuste a responsividade

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="relative w-full bg-gradient-to-br from-[#f0e8dd] via-[#f0e8dd]/70 to-[#e6dccf] py-12 px-4 mt-20 overflow-hidden">
+    <footer className="relative w-full bg-linear-to-br from-[#f0e8dd] via-[#f0e8dd]/70 to-[#e6dccf] py-12 px-4 mt-20 overflow-hidden">
 
       {/* Decoração */}
       <BoneIcon className="absolute left-6 bottom-6 w-10 h-10 text-[#FF7A00]/70 rotate-12 animate-bounce" />
@@ -14,35 +14,35 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto flex flex-col items-center gap-8 text-center">
 
         {/* Título */}
-        <h3 className="text-xl md:text-2xl font-bold text-[#8B4513] flex items-center gap-2">
+        <h3 className="text-sm sm:text-base md:text-2xl font-bold text-[#8B4513] flex flex-wrap items-center justify-center gap-2 text-center">
           Feito com <span className="animate-pulse">🐾</span> por Adopt a Friend
         </h3>
 
-        
+        {/* Redes sociais */}
         <div className="flex items-center gap-10">
 
           <a
             href="https://facebook.com"
             target="_blank"
-            className="group p-3 rounded-full shadow-md hover:scale-110 transition"
+            className="group p-3 rounded-full  shadow-md hover:scale-110 hover:shadow-lg transition"
           >
-            <Facebook className="text-blue-600 group-hover:scale-110 transition" />
+            <Facebook className="text-blue-600" />
           </a>
 
           <a
             href="https://instagram.com"
             target="_blank"
-            className="group p-3 rounded-full hover:scale-110 transition"
+            className="group p-3 rounded-full  shadow-md hover:scale-110 hover:shadow-lg transition"
           >
-            <Instagram className="text-pink-500 group-hover:scale-110 transition" />
+            <Instagram className="text-pink-500" />
           </a>
 
           <a
             href="https://wa.me/5599999999999?text=Olá%20tenho%20interesse%20em%20adotar%20um%20animal"
             target="_blank"
-            className="group p-3 rounded-full hover:scale-110 transition"
+            className="group p-3 rounded-full shadow-md hover:scale-110 hover:shadow-lg transition"
           >
-            <MessageCircle className="text-green-600 group-hover:scale-110 transition" />
+            <MessageCircle className="text-green-600" />
           </a>
 
         </div>
@@ -60,6 +60,7 @@ export default function Footer() {
           </Link>
         </div>
 
+        {/* Copyright */}
         <p
           className="text-sm text-[#FF7A00] font-semibold"
           suppressHydrationWarning
